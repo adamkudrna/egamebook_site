fdfe7d9b376719f4b44ad62430863cd8715a50d8
Adam Kudrna <adam@adamkudrna.cz>
2021-02-12 15:15:15 +0100

    Install Superstatic web server with npm to reduce number of global dependencies

--- serve.sh
+++ serve.sh
@@ -8,10 +8,7 @@ j_pid=$!
 npm run dev &
 n_pid=$!
 
-superstatic --port 5000 &
-f_pid=$!
+echo "caught PIDs: $j_pid, $n_pid"
 
-echo "catched PIDs: $j_pid, $n_pid, $f_pid"
-
-trap "{ kill $j_pid; kill $n_pid; kill $f_pid; exit 0;}" SIGINT
+trap "{ kill $j_pid; kill $n_pid; exit 0;}" SIGINT
 wait
